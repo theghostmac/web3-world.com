a9b13b4e827b56f8702c5a06b65cef195362e937
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-14 04:45:49 -0400

    fucking bug is bugging me, just a refactor in app and main .jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
-import { NavigationBar, Welcome, Footer, Services, Transactions } from "./components";
+// import { NavigationBar, Welcome, Footer, Services, Transactions } from "./components";
+import NavigationBar from './components';
 
 const App = () => {
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-welcome">
-        <Navigate />
+        <NavigationBar />
         <Welcome />
       </div>
       <Services />
       <Transactions />
       <Footer />
     </div>
-  )
+  )+
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+//
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
+const Index = () => {
+     return (
+        <>
+          <App />
+        </>
+     );
+};
+ReactDOM.render(<Index />, document.getElementById("root"));
